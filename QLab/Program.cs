7dba36127a34bf1e3f99fc6d8bc7299e66f5c6ca
noamkam <noamkam12@gmail.com>
2025-01-12 11:59:57 +0200

    אחרי סעיף א

--- QLab/Program.cs
+++ QLab/Program.cs
@@ -17,12 +17,59 @@ public class Program
         qCopy.Insert(13);
         Console.WriteLine(q);
         Console.WriteLine(qCopy);
+        int m = 3;
+        bool IsMagicAnswer = IsMagic(qCopy, m);
+        Console.WriteLine(IsMagicAnswer);
+
+        
     }
+        public static bool IsMagic(Queue<int> q, int m)
+        {
+        Queue<int> qCopy = SetQCopy(q);
+
+
+        //יצרנו העתק של התור ועליו נפעיל את הבדיקה.
+        // התור המקורי נשאר ללא שינוי
+            bool isMagicNum = false;
+            int num1;
+            int num2;
+            int count = 0;
+            num1 = qCopy.Remove();
+            count++;
+            num2 = qCopy.Remove();
+            count++;
+            while (!qCopy.IsEmpty())
+            {
+               
+                if (count == m)
+                {
+                    if (num2 == num1 + qCopy.Head())
+                    {
+                        return true;
+                    
+                    }
+                    else
+                    {
+                    return false;
+                }
+
+                }
+                else
+                {
+                    num1 = num2;
+                    num2 = qCopy.Remove();
+                    count++;
+                    
+            }
+            }
+            return isMagicNum;
+        }
+    
 
     public static Queue<int> SetQCopy(Queue<int> q)
     {
         Queue<int> qCopy = new();
-        Queue<int> qTemp = new();
+        Queue<int> qTemp = new();            
         int currItem;
         //פריקת התור המקורי ויצירת תור העתק ותור זמני
         while (!q.IsEmpty())
@@ -38,6 +85,7 @@ public class Program
         }
         return qCopy;
     }
-        
+
+    
 
 }
